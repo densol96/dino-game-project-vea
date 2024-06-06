de5df3936391118e88c3573051dca44eb7e868c5
Dan4kpromax28 <150060065+Dan4kpromax28@users.noreply.github.com>
2024-06-06 10:55:51 +0300

    Update: class User, h2 database in aplication properties and modified pom

---
+++ back_end/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,3 @@
+
+g
+7src/main/java/lv/vea_dino_game/back_end/model/User.java,8/3/83e2c082721a340c3d77917437f53084eac82e4c
---
+++ back_end/src/main/java/lv/vea_dino_game/back_end/model/user.java
@@ -0,0 +1,34 @@
+package lv.vea_dino_game.back_end.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Table(name = "UserTable")
+@Entity
+
+public class User {
+
+    @Setter(value = AccessLevel.NONE)
+    @Column(name = "Id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @Size(min = 2, max = 30)
+    @Pattern(regexp = "[A-Z]{1}[a-z]+")
+    @Column(name = "Username")
+    private String username;
+
+    @NotNull
+    @Size(min = 2, max = 30)
+    @Column(name = "Password")
+    private String password;
+}

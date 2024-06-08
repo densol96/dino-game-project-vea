249693a97c23501ff0be92dd63bb4a094de3f026
Deniss Solovjovs <deniss11sol@gmail.com>
2024-06-08 13:56:16 +0300

    smal fix

--- back_end/src/main/java/lv/vea_dino_game/back_end/model/CombatResult.java
+++ back_end/src/main/java/lv/vea_dino_game/back_end/model/CombatResult.java
@@ -1,43 +1,43 @@
-// package lv.vea_dino_game.back_end.model;
+package lv.vea_dino_game.back_end.model;
 
-// import jakarta.persistence.*;
-// import jakarta.validation.constraints.Min;
-// import lombok.*;
-// import lv.vea_dino_game.back_end.model.enums.EnumCombatResultType;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.*;
+import lv.vea_dino_game.back_end.model.enums.EnumCombatResultType;
 
-// @Getter
-// @Setter
-// @NoArgsConstructor
-// @ToString
-// @Entity
-// @Table(name = "combat_results")
-// public class CombatResult {
-//     @Setter(value = AccessLevel.NONE)
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     public long id;
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "combat_results")
+public class CombatResult {
+    @Setter(value = AccessLevel.NONE)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public long id;
 
-//     @OneToOne(mappedBy = "id")
-//     @ToString.Exclude
-//     public Combat combat;
+    @OneToOne(mappedBy = "id")
+    @ToString.Exclude
+    public Combat combat;
 
-//     @ManyToOne
-//     @JoinColumn(name = "id")
-//     private Player winner;
+    @ManyToOne
+    @JoinColumn(name = "id")
+    private Player winner;
 
-//     public EnumCombatResultType combatResultType;
+    public EnumCombatResultType combatResultType;
 
-//     @Min(value = 0, message = "Initiator currency change can not be negative number")
-//     public Integer initiatorCurrencyChange = 0;
+    @Min(value = 0, message = "Initiator currency change can not be negative number")
+    public Integer initiatorCurrencyChange = 0;
 
-//     @Min(value = 0, message = "Defender currency change can not be negative number")
-//     public Integer defenderCurrencyChange = 0;
+    @Min(value = 0, message = "Defender currency change can not be negative number")
+    public Integer defenderCurrencyChange = 0;
 
-//     @Min(value = 0, message = "Initiator exp reward can not be negative number")
-//     public Integer initiatorExpReward = 0;
+    @Min(value = 0, message = "Initiator exp reward can not be negative number")
+    public Integer initiatorExpReward = 0;
 
-//     @Min(value = 0, message = "Defender exp reward can not be negative number")
-//     public Integer defenderExpReward = 0;
+    @Min(value = 0, message = "Defender exp reward can not be negative number")
+    public Integer defenderExpReward = 0;
 
 
-// }
+}

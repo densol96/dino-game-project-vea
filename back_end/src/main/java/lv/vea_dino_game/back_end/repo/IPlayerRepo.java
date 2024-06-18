0c919316f5b55eabedc41aff7dfc9cd612529ba8
Dan4kpromax28 <150060065+Dan4kpromax28@users.noreply.github.com>
2024-06-18 18:31:51 +0300

    d

--- back_end/src/main/java/lv/vea_dino_game/back_end/repo/IPlayerRepo.java
+++ back_end/src/main/java/lv/vea_dino_game/back_end/repo/IPlayerRepo.java
@@ -3,8 +3,12 @@ package lv.vea_dino_game.back_end.repo;
 import lv.vea_dino_game.back_end.model.Player;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface IPlayerRepo extends JpaRepository<Player, Integer> {
 
@@ -14,5 +18,7 @@ public interface IPlayerRepo extends JpaRepository<Player, Integer> {
 
     List<Player> findAllByLevel(Integer level);
 
-    Player findByUserId(Integer id1);
+    @Query(value = "SELECT * FROM Players p WHERE p.immune_until < :currentDateTime AND p.id <> :excludeId AND p.level = :level ORDER BY RANDOM() LIMIT 1", nativeQuery = true)
+    Optional<Player> findRandomOpponentByLevelAndWithoutImmunity(@Param("excludeId") int excludeId, @Param("currentDateTime") LocalDateTime currentDateTime, @Param("level") int level);
+
 }

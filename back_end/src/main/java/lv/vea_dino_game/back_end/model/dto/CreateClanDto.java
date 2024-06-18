44ee8d39e0421a2c7955c32770d809ca774d2c24
Dan4kpromax28 <150060065+Dan4kpromax28@users.noreply.github.com>
2024-06-18 18:23:37 +0300

    Some changes try to make Dto and user info from Auth service getMe

---
+++ back_end/src/main/java/lv/vea_dino_game/back_end/model/dto/CreateClanDto.java
@@ -0,0 +1,5 @@
+package lv.vea_dino_game.back_end.model.dto;
+
+public class CreateClanDto {
+
+}
--- back_end/src/main/java/lv/vea_dino_game/back_end/repo/IPlayerRepo.java
+++ back_end/src/main/java/lv/vea_dino_game/back_end/repo/IPlayerRepo.java
@@ -13,4 +13,12 @@ public interface IPlayerRepo extends JpaRepository<Player, Integer> {
     List<Player> findAllByOrderByLevelAsc();
 
     List<Player> findAllByLevel(Integer level);
+<<<<<<< Updated upstream
+=======
+
+    @Query(value = "SELECT * FROM Players p WHERE p.immune_until < :currentDateTime AND p.id <> :excludeId AND p.level = :level ORDER BY RANDOM() LIMIT 1", nativeQuery = true)
+    Optional<Player> findRandomOpponentByLevelAndWithoutImmunity(@Param("excludeId") int excludeId, @Param("currentDateTime") LocalDateTime currentDateTime, @Param("level") int level);
+
+    Player findByUserId(Integer id1);
+>>>>>>> Stashed changes
 }

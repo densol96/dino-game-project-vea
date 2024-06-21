9c32b86a5809e55d296074fad9a718c04908ebde
MihailsKostjuks <kostjuk.misha@gmail.com>
2024-06-21 11:35:59 +0300

    Revert "merge confllict"
    This reverts commit df7eac3856264fe464eeb5e1cd32c0a649fe50a3.

--- back_end/src/main/java/lv/vea_dino_game/back_end/model/Player.java
+++ back_end/src/main/java/lv/vea_dino_game/back_end/model/Player.java
@@ -29,13 +29,6 @@ public class Player {
     @JoinColumn(name = "player_stats")
     private PlayerStats playerStats = new PlayerStats();
 
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "player_combat_stats")
-    private PlayerCombatsStats combatStats = null;
-
-    @OneToOne(cascade = CascadeType.ALL)
-    private Job currentJob = null;
-
     @NotNull(message = "Dino type cannot be null")
     @Enumerated(EnumType.STRING)
     private DinoType dinoType;
@@ -51,8 +44,6 @@ public class Player {
     private Integer level = 1;
 
     private LocalDateTime immuneUntil;
-
-    private LocalDateTime workingUntil = LocalDateTime.now();
 
     @Size(max = 300, message = "Description cannot be longer than 300 chars")
     private String description;
